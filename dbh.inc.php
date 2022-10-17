ad6e9592a6f0268ba96f38d6f102ff0e98728147
Deborsy <108697375+Deborsy@users.noreply.github.com>
2022-10-17 23:48:42 +0100

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,5 +1,6 @@
 <?php 
 
+// Creating a connection to the database
 $conn = mysqli_connect('localhost', 'root', '', 'musicacademy');
 if (!$conn) {
 	die("Connection failed:".mysqli_connect_error());
