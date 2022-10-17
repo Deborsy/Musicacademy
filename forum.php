ba0888364b508d9a2162d7973b38c3339ac9243f
Deborsy <108697375+Deborsy@users.noreply.github.com>
2022-10-17 23:59:50 +0100

    Update forum.php

--- forum.php
+++ forum.php
@@ -10,36 +10,38 @@ include 'comment.inc.php';
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<style type="text/css">
 	body {
-    background-color: yellow;
-    position: relative;
-    -webkit-animation-name: example; /* Chrome, Safari, Opera */
-    -webkit-animation-duration: 4s; /* Chrome, Safari, Opera */
-    -webkit-animation-iteration-count: 1000; /* Chrome, Safari, Opera */
-    animation-name: example;
-    animation-duration: 4s;
-    animation-iteration-count: 1000;
-    border-radius: 25px;
-	}
-	/* Chrome, Safari, Opera */
-@-webkit-keyframes example {
-    0%   {background-color:red;}
-    25%  {background-color:yellow;}
-    50%  {background-color:blue;}
-    75%  {background-color:green;}
-    100% {background-color:red; }
-}
+		/*---------------------------------------------- Animation to change color of the page -------------------------------------------------------*/
+		    background-color: yellow;
+		    position: relative;
+		    -webkit-animation-name: example; /* Chrome, Safari, Opera */
+		    -webkit-animation-duration: 4s; /* Chrome, Safari, Opera */
+		    -webkit-animation-iteration-count: 1000; /* Chrome, Safari, Opera */
+		    animation-name: example;
+		    animation-duration: 4s;
+		    animation-iteration-count: 1000;
+		    border-radius: 25px;
+			}
+			/* Chrome, Safari, Opera */
+		@-webkit-keyframes example {
+		    0%   {background-color:red;}
+		    25%  {background-color:yellow;}
+		    50%  {background-color:blue;}
+		    75%  {background-color:green;}
+		    100% {background-color:red; }
+		}
 
-/* Standard syntax */
-@keyframes example {
-    0%   {background-color:red;}
-    25%  {background-color:yellow;}
-    50%  {background-color:blue;}
-    75%  {background-color:green;}
-    100% {background-color:red;}
-}
-textarea{
-	padding: 15px;
-}
+		/* Standard syntax */
+		@keyframes example {
+		    0%   {background-color:red;}
+		    25%  {background-color:yellow;}
+		    50%  {background-color:blue;}
+		    75%  {background-color:green;}
+		    100% {background-color:red;}
+		}
+		/*---------------------------------- Color animation End -----------------------------------------------*/
+		textarea{
+			padding: 15px;
+		}
 	</style>
 
 </head>
